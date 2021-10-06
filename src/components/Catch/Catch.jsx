73c98ea386489c17d4effece00e8f30c3afc533a
LouisDC98 <louisdacosta98@gmail.com>
2021-10-06 16:40:44 +0200

    Add Catch component

---
+++ src/components/Catch/Catch.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+import chat from '../../img/icon-chat.png';
+import money from '../../img/icon-money.png';
+import security from '../../img/icon-security.png';
+
+function Catch(props) {
+    const { logo, title, subtitle } = props;
+
+    let toto;
+    switch (logo) {
+        case 'chat':
+            toto = chat;
+            break;
+        case 'money':
+            toto = money;
+            break;
+        case 'security':
+            toto = security;
+            break;
+    }
+
+    return (
+        <BlocCatch>
+            <ImgCatch src={toto} />
+            <TitleCatch>{title}</TitleCatch>
+            <SubtitleCatch>{subtitle}</SubtitleCatch>
+        </BlocCatch>
+    );
+}
+
+const BlocCatch = styled.div`
+    background-color: white;
+    text-align: center;
+    width: 100%;
+    padding: 40px 0;
+    @media (min-width: 920px) {
+        padding: 40px;
+        width: 28%;
+    }
+`;
+
+const ImgCatch = styled.img`
+    border: 10px solid #00bc77;
+    border-radius: 50%;
+    padding: 16px;
+    height: 100px;
+`;
+
+const TitleCatch = styled.p`
+    color: #222;
+    font-size: 1.25rem;
+    font-weight: bold;
+    margin-bottom: 16px;
+`;
+
+const SubtitleCatch = styled.p`
+    color: #2c3e50;
+`;
+
+export default Catch;
--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../../components/Header/Header';
 import Slogan from '../../components/Slogan/Slogan';
+import Catch from '../../components/Catch/Catch';
 
 function homePage() {
     return (
         <div>
             <Header sign={'in'} />
             <Slogan />
+            <CatchShape>
+                <Catch
+                    logo={'chat'}
+                    title={'You are our #1 priority'}
+                    subtitle={
+                        'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.'
+                    }
+                />
+                <Catch
+                    logo={'money'}
+                    title={'More savings means higher rates'}
+                    subtitle={'The more you save with us, the higher your interest rate will be!'}
+                />
+                <Catch
+                    logo={'security'}
+                    title={'Security you can trust'}
+                    subtitle={
+                        'We use top of the line encryption to make sure your data and money is always safe.'
+                    }
+                />
+            </CatchShape>
         </div>
     );
 }
+
+const CatchShape = styled.div`
+    margin-top: -4px;
+    @media (min-width: 920px) {
+        display: flex;
+        justify-content: space-around;
+    }
+`;
 
 export default homePage;
